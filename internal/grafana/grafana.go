e6ac3190ca38f97253e51971b54e51ccaec343c8
Roman Ashaev <roman.ashaev@itms.tech>
2023-10-12 21:59:24 +0300

    remove unsed struct

--- internal/grafana/grafana.go
+++ internal/grafana/grafana.go
@@ -6,9 +6,6 @@ import (
 	gapi "github.com/grafana/grafana-api-golang-client"
 )
 
-type SyncUser struct {
-}
-
 func New(url url.URL, config gapi.Config) (*gapi.Client, error) {
 	return gapi.New(url.String(), config)
 }
